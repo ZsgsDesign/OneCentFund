140f8f737b6f823b37eced88238f520d4fdb110c
LLAP <ospiper@foxmail.com>
2016-08-31 02:41:38 +0800

    关闭调试 删除不必要配置

--- protected/config.php
+++ protected/config.php
@@ -30,28 +30,6 @@ $domain = array(
 		),
 	),
 	"1cf.co" => array( //线上配置
-		'debug' => 1,
-		'mysql' => array(
-				'MYSQL_HOST' => 'localhost',
-				'MYSQL_PORT' => '3306',
-				'MYSQL_USER' => 'root',
-				'MYSQL_DB'   => '1cf',
-				'MYSQL_PASS' => 't3dv95my',
-				'MYSQL_CHARSET' => 'utf8',
-		),
-	),
-	"www.1cf.co" => array( //线上配置
-		'debug' => 1,
-		'mysql' => array(
-				'MYSQL_HOST' => 'localhost',
-				'MYSQL_PORT' => '3306',
-				'MYSQL_USER' => 'root',
-				'MYSQL_DB'   => '1cf',
-				'MYSQL_PASS' => 't3dv95my',
-				'MYSQL_CHARSET' => 'utf8',
-		),
-	),
-	"1cf.l-xiao.com" => array( //线上配置
 		'debug' => 0,
 		'mysql' => array(
 				'MYSQL_HOST' => 'localhost',
@@ -62,6 +40,17 @@ $domain = array(
 				'MYSQL_CHARSET' => 'utf8',
 		),
 	),
+	"www.1cf.co" => array( //线上配置
+		'debug' => 0,
+		'mysql' => array(
+				'MYSQL_HOST' => 'localhost',
+				'MYSQL_PORT' => '3306',
+				'MYSQL_USER' => 'root',
+				'MYSQL_DB'   => '1cf',
+				'MYSQL_PASS' => 't3dv95my',
+				'MYSQL_CHARSET' => 'utf8',
+		),
+	)
 );
 // 为了避免开始使用时会不正确配置域名导致程序错误，加入判断
 if(empty($domain[$_SERVER["HTTP_HOST"]])) die("配置域名不正确，请确认".$_SERVER["HTTP_HOST"]."的配置是否存在！");
